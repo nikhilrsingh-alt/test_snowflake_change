d9bcd891b7cb7e399182fa9517f604f008a1e1b6
nikhilrsingh-alt <nikhil.r.singh@kipi.ai>
2025-08-29 23:18:12 +0530

    Update V1.0.0__create_change_history_table.sql

--- migrations/V1.0.0__create_change_history_table.sql
+++ migrations/V1.0.0__create_change_history_table.sql
@@ -1,6 +1,6 @@
 -- This script is often the first one, creating the table schemachange uses to track state.
 -- Note: schemachange can create this automatically, but defining it yourself is good practice.
-CREATE TABLE IF NOT EXISTS DEMO_DB.PUBLIC.CHANGE_HISTORY (
+CREATE TABLE IF NOT EXISTS TEST.GITHUB.CHANGE_HISTORY (
     VERSION VARCHAR,
     DESCRIPTION VARCHAR,
     SCRIPT VARCHAR,
